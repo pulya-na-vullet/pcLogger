ccbd23758980120f0d04e49f3bc67fb197b47fab
huaweiDESC <grig-dmitriy@mail.ru>
2023-04-11 23:34:07 +0300

    3 minimize refactoring

--- realData/Send_to_telegramm.py
+++ realData/Send_to_telegramm.py
@@ -2,15 +2,16 @@ import requests
 import datetime
 import telegram
 
-# Установка переменных
-now = datetime.datetime.now()
 TOKEN = "6270028467:AAFdqmtb-gmpR4J4YQhbBZtfZ4N1SeK5VJw"
 chat_id = "781078907"
-message = "Запуск твоего компьютера:"
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-url2 = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={now}"
-
-# Отправка сообщений в чат
-print(requests.get(url).json())
-print(requests.get(url2).json())
 bot = telegram.Bot(token=TOKEN)
+
+# Отправка сообщения
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}"
+    requests.get(url)
+
+# Пример использования функций
+now = datetime.datetime.now()
+send_message("Запуск твоего компьютера:")
+send_message(now)
