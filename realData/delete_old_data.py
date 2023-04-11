d13186b37c73d9accef15f81516d05fb3086933b
huaweiDESC <grig-dmitriy@mail.ru>
2023-04-12 00:22:47 +0300

    global refactoring

--- realData/delete_old_data.py
+++ realData/delete_old_data.py
@@ -1,6 +1,20 @@
 import os
 import time
-import Send_to_telegramm
+import requests
+import datetime
+import telegram
+
+TOKEN = "6270028467:AAFdqmtb-gmpR4J4YQhbBZtfZ4N1SeK5VJw"
+chat_id = "781078907"
+bot = telegram.Bot(token=TOKEN)
+
+# Отправка сообщения
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}"
+    requests.get(url)
+
+# Пример использования функций
+now = datetime.datetime.now()
 
 path = "D:/"
 txt_files = [f for f in os.listdir(path) if f.endswith('.txt')]
@@ -12,5 +26,6 @@ for file in txt_files:
         file_age = time.time() - os.path.getmtime(full_file_path)
         if file_age > threshold_days * 24 * 60 * 60:
             os.remove(full_file_path)
-            Send_to_telegramm.send_message(f"File {full_file_path} has been deleted")
-Send_to_telegramm.send_message("Файлы на локальном ПК не были удалены")
+            send_message(f"File {full_file_path} has been deleted")
+send_message("Файлы на локальном \nПК небыли удалены")
+
--- realData/pinGG.bat
+++ realData/pinGG.bat
@@ -3,3 +3,5 @@
 
 timeout 30
 python D:\Send_to_telegramm.py %*
+temeout 2
+python D:\delete_old_data.py %*
