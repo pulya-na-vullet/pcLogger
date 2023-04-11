da6d51d0f19d89aeffd4cdcf61af69767bd25e09
huaweiDESC <grig-dmitriy@mail.ru>
2023-04-12 00:00:02 +0300

    last commit today

--- realData/delete_old_data.py
+++ realData/delete_old_data.py
@@ -1,16 +1,6 @@
 import os
 import time
-import requests
-import datetime
-import telegram
-
-TOKEN = "6270028467:AAFdqmtb-gmpR4J4YQhbBZtfZ4N1SeK5VJw"
-chat_id = "781078907"
-bot = telegram.Bot(token=TOKEN)
-
-def send_message(text):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={text}"
-    requests.get(url)
+import Send_to_telegramm
 
 path = "D:/"
 txt_files = [f for f in os.listdir(path) if f.endswith('.txt')]
@@ -22,11 +12,5 @@ for file in txt_files:
         file_age = time.time() - os.path.getmtime(full_file_path)
         if file_age > threshold_days * 24 * 60 * 60:
             os.remove(full_file_path)
-            print(f"File {full_file_path} has been deleted")
-    else:
-       print('Nothing to delete')
-
-
-# Пример использования функций
-now = datetime.datetime.now()
-send_message("Были уалены старые файлы")
+            Send_to_telegramm.send_message(f"File {full_file_path} has been deleted")
+Send_to_telegramm.send_message("Файлы на локальном ПК не были удалены")
