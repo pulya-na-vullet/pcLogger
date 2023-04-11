6737f1ab7225afd5ed307593c17bffe0ccc8f297
huaweiDESC <grig-dmitriy@mail.ru>
2023-04-11 23:20:30 +0300

    refactoring code for minimizing 2

--- реал дата/First_step.py
+++
@@ -1,5 +0,0 @@
-#Скрипт поиска ID чата обзения между клментом и телеграмм ботом
-import requests
-TOKEN = "Уникальный токен от FatherBot"
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-print(requests.get(url).json())
