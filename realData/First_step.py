5e9bf5ef3ce8437fd6722c008d99ab23653e44da
huaweiDESC <grig-dmitriy@mail.ru>
2023-04-11 23:29:42 +0300

    refactoring to minimize 2

--- realData/First_step.py
+++ realData/First_step.py
@@ -1,5 +1,5 @@
 #Скрипт поиска ID чата обзения между клментом и телеграмм ботом
 import requests
-TOKEN = "Уникальный токен от FatherBot"
+TOKEN = "6270028467:AAFdqmtb-gmpR4J4YQhbBZtfZ4N1SeK5VJw"
 url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 print(requests.get(url).json())
--- realData/Send_to_telegramm.py
+++ realData/Send_to_telegramm.py
@@ -1,35 +1,16 @@
 import requests
 import datetime
 import telegram
-import os
-from datetime import date
 
+# Установка переменных
 now = datetime.datetime.now()
 TOKEN = "6270028467:AAFdqmtb-gmpR4J4YQhbBZtfZ4N1SeK5VJw"
 chat_id = "781078907"
 message = "Запуск твоего компьютера:"
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
 url2 = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={now}"
-print(requests.get(url).json()) # Эта строка отсылает сообщение
+
+# Отправка сообщений в чат
+print(requests.get(url).json())
 print(requests.get(url2).json())
-
 bot = telegram.Bot(token=TOKEN)
-
-def send_files(files):
-    for file in files:
-        with open(file, 'rb') as f:
-            bot.send_document(chat_id=chat_id, document=f)
-
-def get_today_files():
-    today = date.today()
-    folder = 'D:/'
-    files = os.listdir(folder)
-    today_files = []
-    for file in files:
-        if os.path.isfile(os.path.join(folder, file)) and file.endswith('.txt') and file.startswith('Start_pc_worc_'):
-            today_files.append(os.path.join(folder, file))
-    return today_files
-
-if __name__ == '__main__':
-    files = get_today_files()
-    send_files(files)
